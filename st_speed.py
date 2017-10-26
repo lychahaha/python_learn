f2a7136399c9524d71d80daa735f7e4a33ec838d
ljhandlwt <1412441716@qq.com>
2017-10-26 22:33:11 +0800

    speed:add line_profile

--- st_speed.py
+++ st_speed.py
@@ -1,11 +1,10 @@
 #jh:proc-base-typical,key-type
 import cProfile
-import profile
+#import profile
 import pstats
 
 def main():
     xxx
-
 
 #typical
 if __name__ == '__main__':
@@ -14,8 +13,6 @@ if __name__ == '__main__':
     p = pstats.Stats('result.txt')
     p.sort_stats('cumulative').print_stats(10)
     p.print_callees("main")
-
-
 #full
 if __name__ == '__main__':
     #run
@@ -41,3 +38,40 @@ if __name__ == '__main__':
 
     p.print_callers("xxx")#打印调用了这个函数的函数
     p.print_callees("xxx")#打印这个函数调用了的函数
+
+
+
+#test.py
+@profile
+def main():
+    xxx
+main()
+
+#typical
+kernprof -l -v test.py
+
+kernprof -l -o xxx.lprof test.py
+python -m line_profile xxx.lprof
+
+#full
+kernprof:
+-l #使用line_profile(一般都要)
+-v #程序结束后打印结果
+-o xxx.lprof #结果保存在文件里
+
+#line:行号
+#hits:调用次数
+#time:总时间
+#per hit:平均时间
+#%time:总时间百分比
+#line contents:该行内容
+
+
+#保证profile不会报错
+if 'builtins' not in dir() or not hasattr(builtins, 'profile'):
+    import builtins
+    def profile(func):
+        def inner(*args, **kwargs):
+            return func(*args, **kwargs)
+        return inner
+    builtins.__dict__['profile'] = profile
