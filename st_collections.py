53bd388fed782cad269a7001c8a0ca75a19c3769
ljhandlwt <1412441716@qq.com>
2019-02-05 11:35:17 +0800

    modify collections

--- st_collections.py
+++ st_collections.py
@@ -1,25 +1,76 @@
 #jh:class-base,lazy-type
 from collections import *
 
-#计数器(可以当成字典用)
-c = Counter(l)
+'''计数器(可以当成字典用)'''
+c = Counter([1,2,3,2,4,1])
+c = Counter('43242523423')
+c = Counter(a=3, b=4)
+c = Counter({'a':3,'b':2})
+
 ##top-k
 #返回(ele,cnt)的列表
 c.most_common(k)
-##再添加一个列表参与计数
-c.update(l)
+c.most_common() #返回全部
 
-#双端队列(可以当成列表用)
+##再添加一个列表/字符串...参与计数
+c.update(l)
+##反向计数(可以变到负)
+c.subtract(l)
+
+
+'''双端队列(可以当成列表用,主要是时间复杂度不同)'''
 d = deque()
 d = deque(l)
-##push and pop
+
+##push
 d.append(x)
 d.appendleft(x)
-x = d.pop()
-x = d.popleft()
 d.extend(l)
 d.extendleft(l)
+##pop
+x = d.pop()
+x = d.popleft()
 
-#带默认参数的字典
-d = defaultdict(list) #值是一个列表
-d = defaultdict(int) #值默认是0
+##rotate
+d.rotate(3) #把队尾的3个元素旋转到队首
+d.rotate(-3) #把队首的3个元素旋转的队尾
+
+
+'''命名元组(可以认为是快速写结构体)'''
+Point = namedtuple('Point', ['x','y'])
+p = Point(3,4)
+
+##索引
+p[0]
+p.x
+
+##下面和tuple一样
+p.count(3)
+p.index(3)
+
+
+'''多个字典合并成一个字典(可以认为是list(dict))'''
+#d不会深拷贝d1和d2,所以d1,d2修改后d也会修改
+d1 = {1:1,2:2}
+d2 = {1:4,3:3}
+d = ChainMap(d1,d2)
+
+##读(从前往后查询)
+d[1] #1
+d[3] #3
+##写(只修改第一个dict,包括其他pop之类的修改操作)
+d[1] = 2
+
+##获取所有字典
+d.maps #[d1,d2]
+
+##增加新字典(放在最前)
+d = d.new_child()
+d = d.new_child(d0)
+##删除字典(只能删除最前)
+d = d.parents
+
+
+'''继承'''
+#最好继承它们,而不是直接继承list,dict,str
+UserList,UserDict,UserString
--- st_dict.py
+++ st_dict.py
@@ -6,9 +6,10 @@ a = dict(one=1, two=2)
 a = dict.fromkeys(seq,val)#所有值都为val,没有提供val则是None
 
 #获取值
-print a['one']
-print a.get('one')
-print a.setdefault(key,val)#若键存在,则返回对应的值.否则返回val,并且插入(key,val)
+x = a['one']
+x = a.get('one') #没有这个键则返回None
+x = a.get('one', 0) #没有这个键则返回0
+x = a.setdefault(key,val)#若键存在,则返回对应的值.否则返回val,并且插入(key,val)
 
 #修改值
 a['one'] = 2
@@ -27,25 +28,27 @@ for key in a:
 a.pop(key)#删除key对应的键值对,并返回其值
 a.pop(key,default)#没找到这个键则返回默认值
 a.popitem()#随机删除并返回一个键值对,字典为空时抛出异常
+a.clear()#清空
 
 #dict->list
 b = list(a.items())#键值对
 b = list(a.keys())#键
 b = list(a.values())#值
 
-#dict->iter
-b = a.iteritems()#键值对
-b = a.iterkeys()#键
-b = a.itervalues()#值
-for key,val in a.iteritems():
-	print key,val
-for key in a.iterkeys():
-	print key
-for val in a.itervalues():
-	print val
 
-#拷贝
-b = a.copy()
+from collections import defaultdict,OrderedDict
 
-#清空
-a.clear()
+#defaultdict
+#自动插入新键的字典
+d = defaultdict(list) #值是一个列表
+d = defaultdict(int) #值默认是0
+d[3].append(4) #没有3时自动插入(3,[])
+d[4] += 1 #没有4时自动插入(4,0)
+
+
+#OrderedDict
+#键的遍历按插入顺序排序的字典(相当于可快速索引的队列)
+d = OrderedDict()
+
+d.move_to_end(key) #把key从队列中拿出,并放到队尾
+d.move_to_end(key, last=False) #放到队首
