3171c17181de3b429deb1510dd19385240ed1288
ljhandlwt <1412441716@qq.com>
2019-02-05 14:43:39 +0800

    modify args

--- st_args.py
+++ st_args.py
@@ -26,8 +26,11 @@ if __name__ == '__main__':
 	parser.add_argument("--verbosity", help="xxx")
 
 	##定义01名字参数(选填)
-	#python a.py -v
+	#python a.py -k
 	parser.add_argument("-k", action='store_true')
+	parser.add_argument("-k", action='store_false')
+	#更一般的01名字参数
+	parser.add_argument("-k", action='store_const', const=42)
 
 	##定义参数类型,默认值,值域
 	parser.add_argument("-c", type=int)
@@ -38,6 +41,23 @@ if __name__ == '__main__':
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument("-a")
 	group.add_argument("-b")
+
+	#多值参数
+	##指定值个数
+	##python a.py -s 1 2
+	parser.add_argument("-s", nargs=2) 
+	##不定值个数(至少一个)
+	##python a.py -s 1 2 3 
+	parser.add_argument("-s", nargs='+')
+	##不定值个数(可以零个)
+	##python a.py -s
+	parser.add_argument("-s", nargs='*')
+	##0个或1个值
+	##python a.py -s 1
+	parser.add_argument("-s", nargs='?')
+
+	#换名字(为了调用的方便性和代码的可读性)
+	parser.add_argument("-v", dest='version') #args.version
 
 	#获取参数表
 	args = parser.parse_args()
@@ -52,5 +72,4 @@ if __name__ == '__main__':
 	#有定义类型的,按照定义的类型
 	#01参数是bool类型
 
-	#传递多值时,可以加双引号变成一个字符串,再在py里处理
-	#python a.py -k "a b c"
+	#传递多值时,参数是个list
