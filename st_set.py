33632ae57713f105467f7b6ecda14c776b27637a
ljhandlwt <1412441716@qq.com>
2019-02-05 12:06:11 +0800

    modify str

--- st_set.py
+++ st_set.py
@@ -1,42 +1,38 @@
 #jh:func-base-mixsimilar,key-type
-#coding=utf-8
 
 a = set()
 a = set([1,2,3])
 
 #插入
 a.add(val)
-
 #删除
 a.remove(val)#没找到会抛出异常
 a.discard(val)#没找到不会抛出异常
 a.pop()#随机删除并返回一个元素
 
 #集合运算
-b = a.difference(iter)#差集
-b = a.intersection(iter)#交集
-b = a.symmetric_difference(iter)#对称差集
-b = a.union(iter)#并集
+b = a.difference(iter_obj)#差集
+b = a.intersection(iter_obj)#交集
+b = a.symmetric_difference(iter_obj)#对称差集
+b = a.union(iter_obj)#并集
 #运算符
 c = a - b #差集
 c = a & b #交集
 c = a | b #并集
 c = a ^ b #对称差集
 #原地更新
-a.difference_update(iter)#差集并更新
-a.intersection_update(iter)#交集并更新
-a.symmetric_difference_update(iter)#对称差集并更新
-a.update(iter)#并集并更新
+a.difference_update(iter_obj)#差集并更新
+a.intersection_update(iter_obj)#交集并更新
+a.symmetric_difference_update(iter_obj)#对称差集并更新
+a.update(iter_obj)#并集并更新
 
-
-
-print a.isdisjoint(iter)#判断是否没有交集
-print a.issubset(iter)#判断是否是子集
-print a.issuperset(iter)#判断是否是父集
+#判断
+a.isdisjoint(iter_obj)#判断是否没有交集
+a.issubset(iter_obj)#判断是否是子集
+a.issuperset(iter_obj)#判断是否是父集
 
 #拷贝
-b = a.copy()#浅拷贝
-b = copy.deepcopy(a)#深拷贝
+b = a.copy()
 
 #清空
 a.clear()
--- st_str.py
+++ st_str.py
@@ -1,81 +1,82 @@
 #jh:func-base-mixsimilar,full-type
-#coding=utf-8
+
+a = 'strSTR123str'
+
+#合并
+'.'.join(a.split('t'))
+#分割
+a.split()#这样相当于没有分割符
+a.split('t')#分割符,分割符会被删掉
+a.split('t', 1)#最多分割次数
+a.splitlines()#相当于a.split('\n')
+
+#查找
+a.find('str')#返回头下标,没找到返回-1
+a.find('str', 3, 6)#在给定范围[beg,end)找
+a.index('str')#返回头下表,没找到抛出异常
+a.partition('123')#原字符串拆成三部分返回,(beg,target,end),如果没找到则返回(src,'','')
+#替换
+a.replace('123', '456')
+a.replace('str', 'haha', 1)#最多替换次数
+#计数
+a.count('str')
+a.count('str', 3, 6)#在给定范围[beg,end)计数
 
 #format
 s = "{}+{}={}"
 s.format(2,3,2+3)
-
+##例子
 s = "{1}+{0}={2}" #设置参数顺序
+s = "{a}+{b}={c}" #使用名字索引, s.format(a=1,b=2,c=3)
 s = "{0:4}" #控制宽度(前面是参数顺序)
 s = "{:.3}" #小数位数
 s = "{:<7.3}" #居左,宽度7,小数位数3(>是居右,^是居中)
-
-
-a = 'strSTR123str'
-#合并
-print '.'.join(a.split('t'))
-#分割
-print a.split()#这样相当于没有分割符
-print a.split('t')#分割符,分割符会被删掉
-print a.split('t', 1)#最多分割次数
-
-#查找
-print a.find('str')#返回头下标,没找到返回-1
-print a.find('str', 3, 6)#在给定范围[beg,end)找
-print a.index('str')#返回头下表,没找到抛出异常
-#替换
-print a.replace('123', '456')
-print a.replace('str', 'haha', 1)#最多替换次数
-#计数
-print a.count('str')
-print a.count('str', 3, 6)#在给定范围[beg,end)计数
-
-#转大小写
-print a.upper()
-print a.lower()
-print a.casefold()#所有语言的小写
-print a.swapcase()#所有语言的反转
-
-#str转int,带进制转换
-print string.atoi('FF',16)
-print string.atoi('0xff',0)#基数为0时检查字符串前缀
-#str转float
-print string.atof('1.23e5')
-
-#规范
-b = a.capitalize()#首字母大写,后面小写
-
-#编码解码
-b = a.decode('utf-8')
-b = a.encode('utf-8')
-
-#判断字母
-print a.isalpha()#全字母(不止英文)
-print a.islower()#全小写
-print a.isupper()#全大写
-print a.isspace()#全空白符
-#判断数字
-print a.isdecimal()#判断普通数字,全角数字
-print a.isdigit()#判断普通数字,全角数字,(byte数字),罗马数字
-print a.isnumeric()#判断普通数字,全角数字,罗马数字,汉字数字
-print a.isalnum()#isdecimal||isdigit||isnumeric
-#其他判断
-print a.isprintable()#能否打印
-
-#align
-b = a.center(5,' ')#居中后字符串宽度,填充字符
-b = a.ljust(5, ' ')#居左后字符串宽度,填充字符
-b = a.rjust(5, ' ')#居右后字符串宽度,填充字符
-
-#tab->space
-print a.expandtabs(tabsize=8)
-
-#判断首尾
-print a.startswith('xxx')
-print a.endswith('xxx')
 
 #去掉首尾符号
 b = a.strip()#去掉首尾空白符
 b = a.strip('k')#去掉首尾的k
 b = a.lstrip()#去掉首
 b = a.rstrip()#去掉尾
+
+#转大小写
+a.upper()
+a.lower()
+a.casefold()#所有语言的小写
+a.swapcase()#所有语言的反转
+b = a.capitalize()#首字母大写,后面小写
+
+#判断首尾
+a.startswith('xxx')
+a.endswith('xxx')
+#判断字母
+a.isalpha()#全字母(不止英文)
+a.islower()#全小写
+a.isupper()#全大写
+a.isspace()#全空白符
+#判断数字
+a.isdecimal()#判断普通数字,全角数字
+a.isdigit()#判断普通数字,全角数字,(byte数字),罗马数字
+a.isnumeric()#判断普通数字,全角数字,罗马数字,汉字数字
+a.isalnum()#isdecimal||isdigit||isnumeric
+#其他判断
+a.isprintable()#能否打印
+
+#align
+b = a.center(5,' ')#居中后字符串宽度,填充字符
+b = a.ljust(5, ' ')#居左后字符串宽度,填充字符
+b = a.rjust(5, ' ')#居右后字符串宽度,填充字符
+b = a.zfill(10)#左对齐,前面补零(针对数字)
+
+#tab->space
+a.expandtabs(tabsize=8)
+
+#格式转换
+#char<->int
+x = ord('3')
+s = chr(51)
+#16进制<->int
+x = int('0x3', base=16)
+s = hex(10)
+#byte<->str
+s = b.decode('utf-8')
+b = s.encode('utf-8')
